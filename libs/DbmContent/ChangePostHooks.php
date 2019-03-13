47432ca5495418703220b8b3b5beba6f3826812a
Mattias Ekendahl <mattias@developedbyme.com>
2019-03-13 13:04:58 +0100

    Update: generating unique slugs when autoadding terms

--- libs/DbmContent/ChangePostHooks.php
+++ libs/DbmContent/ChangePostHooks.php
@@ -21,6 +21,7 @@
 			
 			$this->register_hook_for_type('dbm/relation', 'hook_set_relation');
 			$this->register_hook_for_type('dbm/autoDbmContent', 'hook_auto_dbm_content');
+			$this->register_hook_for_type('dbm/addTermFromOwner', 'hook_add_term_from_owner');
 			
 		}
 		
@@ -42,6 +43,19 @@
 			$dbm_content_object = dbm_get_content_object_for_type_and_relation($post_id);
 			
 			do_action('dbm_content/parse_dbm_content', $dbm_content_object, $post_id, $post);
+		}
+		
+		public function hook_add_term_from_owner($data, $post_id) {
+			//echo("\DbmContent\ChangePostHooks::hook_add_term_from_owner<br />");
+			
+			$owner_id = $data['value'];
+			
+			$owner = get_post($owner_id);
+			if($owner) {
+				$meta_name = 'dbm_relation_term_'.$data['group'];
+				$term_id = (int)get_post_meta($owner_id, $meta_name, true);
+				wp_add_object_terms($post_id, array($term_id), 'dbm_relation');
+			}
 		}
 		
 		public static function test_import() {
