441d6e9eac41bf9ffaecbc02aef159252444b4b0
Mattias Ekendahl <mattias@developedbyme.com>
2019-04-01 16:51:37 +0200

    Update: added change to put the item an admin grouping

--- libs/DbmContent/ChangePostHooks.php
+++ libs/DbmContent/ChangePostHooks.php
@@ -21,6 +21,7 @@
 			
 			$this->register_hook_for_type('dbm/relation', 'hook_set_relation');
 			$this->register_hook_for_type('dbm/autoDbmContent', 'hook_auto_dbm_content');
+			$this->register_hook_for_type('dbm/inAdminGrouping', 'hook_in_admin_grouping');
 			
 		}
 		
@@ -42,6 +43,28 @@
 			$dbm_content_object = dbm_get_content_object_for_type_and_relation($post_id);
 			
 			do_action('dbm_content/parse_dbm_content', $dbm_content_object, $post_id, $post);
+		}
+		
+		public function hook_in_admin_grouping($data, $post_id) {
+			
+			$path = 'admin-grouping/'.$data['value'];
+			
+			var_dump($path);
+			
+			$parent = dbm_get_post_id_by_type_and_relation('any', array($path));
+			
+			var_dump($parent);
+			
+			if($parent) {
+				$args = array(
+					'ID' => $post_id,
+					'post_parent' => $parent
+				);
+				
+				var_dump($args);
+				
+				wp_update_post($args);
+			}
 		}
 		
 		public static function test_import() {
